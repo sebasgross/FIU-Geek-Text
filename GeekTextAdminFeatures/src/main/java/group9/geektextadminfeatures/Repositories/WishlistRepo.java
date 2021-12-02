7dcf118b1b4e3f2e9a372834e8567b42a09ef87a
Sebastian Grossmann <45401557+sebasgross@users.noreply.github.com>
2021-12-01 22:41:21 -0500

    Delete GeekTextAdminFeatures directory

--- GeekTextAdminFeatures/src/main/java/group9/geektextadminfeatures/Repositories/WishlistRepo.java
+++
@@ -1,7 +0,0 @@
-package group9.geektextadminfeatures.Repositories;
-
-import group9.geektextadminfeatures.Entities.Wishlist;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WishlistRepo extends MongoRepository<Wishlist, Integer> {
-}
